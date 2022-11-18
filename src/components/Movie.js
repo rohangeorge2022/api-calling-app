364a310ffaadfea5a7b03a816008e68b9d36663e
Rohan T George <81823190+rohantgeorge@users.noreply.github.com>
2022-11-18 07:14:46 -0500

    modal edits initial

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,16 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Movie = ({movie}) => (
-  <div className="movie">
-    <img src={"https://image.tmdb.org/t/p/original/" + movie.poster_path} alt={movie.title} />
-    <div className="movie-info">
-      <h3>{movie.title}</h3>
+const Movie = ({ movie }) => {
+  const [show, setShow] = useState(false);
+
+  const handleShow = () => setShow(true);
+  const handleClose = () => setShow(false);
+
+  return (
+    <div className="movie">
+      <img
+        src={"https://image.tmdb.org/t/p/original/" + movie.poster_path}
+        alt={movie.title}
+      />
+      <div className="movie-info">
+        <div className="rating">Rating</div>
+        <div className="title">
+          <h3>{movie.title}</h3>
+        </div>
+        <div className="movie-footer">
+          <a href="javascript:void(0);" className="trailer-btn">
+            Trailer
+          </a>
+          <button type="button" onClick={handleShow} className="movie-modal">Info</button>
+        </div>
+      </div>
     </div>
-    {/* <div className="overview">
-      <h3>Overview:</h3>
-      <p>{movie.overview}</p>
-    </div> */}
-  </div>
-);
+  );
+};
 
 export default Movie;
